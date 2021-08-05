2b8d2d25acd1dd6c98b70163f9cb599715477174
willyamsann <willyam.oliveira@dellead.com>
2021-08-05 14:27:17 -0300

    Aula 3 - Inclusão da categoria no projeto da Aula2 MVC Gastos

--- EstoqueAtalaia/Migrations/20210804132835_FIrstDb.Designer.cs
+++ EstoqueAtalaia/Migrations/20210804132835_FIrstDb.Designer.cs
@@ -4,14 +4,16 @@ using EstoqueAtalaia.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EstoqueAtalaia.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20210804132835_FIrstDb")]
+    partial class FIrstDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EstoqueAtalaia/Migrations/20210804132835_FIrstDb.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EstoqueAtalaia.Migrations
+{
+    public partial class FIrstDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrdemDeServicoViewModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameCliente = table.Column<string>(nullable: true),
+                    EmailCliente = table.Column<string>(nullable: true),
+                    TelefoneCliente = table.Column<string>(nullable: true),
+                    Aparelho = table.Column<string>(nullable: true),
+                    IMEI = table.Column<string>(nullable: true),
+                    Detalhes = table.Column<string>(nullable: true),
+                    Reclamacao = table.Column<string>(nullable: true),
+                    Diagnostico = table.Column<string>(nullable: true),
+                    Garantia = table.Column<string>(nullable: true),
+                    ValorServico = table.Column<float>(nullable: false),
+                    ValorPeca = table.Column<float>(nullable: false),
+                    Sinal = table.Column<float>(nullable: false),
+                    Resta = table.Column<float>(nullable: false),
+                    ValorTotal = table.Column<float>(nullable: false),
+                    CheckList = table.Column<string>(nullable: true),
+                    DataAbertura = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrdemDeServicoViewModel", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrdemDeServicoViewModel");
+        }
+    }
+}
